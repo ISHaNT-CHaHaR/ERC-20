bdbaa20f7c68d7f45c02b8e6be213fecb7b84ad1
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-05-16 16:47:26 +0530

    Created ERC20

---
+++ contracts/Token20.sol
@@ -0,0 +1,4 @@
+pragma solidity >=0.4.24;
+
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Capped.sol";
